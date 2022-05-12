5f51111361e3625bb44cb6b5d2f84014c0d46bd6
Aadit Gupta <spacegiant21@gmail.com>
2022-05-11 21:13:36 -0700

    feat: Firebase Google Authentication

--- main.py
+++ main.py
@@ -18,5 +18,9 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run( debug=True)

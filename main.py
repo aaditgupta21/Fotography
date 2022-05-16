e7d1123495a69e155b027ec25e374884ddae4ede
Rohit De <de.rohit01@gmail.com>
2022-05-16 14:26:59 -0700

    pages

--- main.py
+++ main.py
@@ -22,5 +22,13 @@ def about():
 def login():
     return render_template("login.html")
 
+@app.route('/events')
+def events():
+    return render_template("events.html")
+
+@app.route('/shop')
+def shop():
+    return render_template("shop.html")
+
 if __name__ == "__main__":
     app.run( debug=True)
